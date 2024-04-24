ac1888ebc1b29aafedc6258d5654dc7bc6331513
jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2024-04-24 06:34:19 -0600

    added a function to add new books to the library

--- btnEvents.js
+++ btnEvents.js
@@ -4,11 +4,16 @@ const btnNewBook = document.querySelector("#btnNewBook");
 const btnAddBook = document.querySelector("#btnAddBook");
 const library = document.querySelector(".library");
 
+
 //#region    
 
 //#region 
 
 
+const function CreateBook(params) {
+  const book = document.createElement("div");
+  book.setAttribute("class","book");
+}
 
 btnDelete.addEventListener("click", function(){
     const child = document.getElementById("book1");
