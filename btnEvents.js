ad4b48979d991bd31a62400a6531db579c7b3d8d
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2024-04-25 13:03:26 -0600

    push awa

--- btnEvents.js
+++ btnEvents.js
@@ -1,14 +1,19 @@
 let controlBooksId = [];
 let idCounter = 0;
+
+let bookName = "";
+let bookAuthor = "";
+let bookPages = 0;
+let bookSynopsis = "";
+let isAvailable = false;
+
+
 const btnDelete = document.querySelector("#btnDelete");
 const btnNewBook = document.querySelector("#btnNewBook");
 const btnAddBook = document.querySelector("#btnAddBook");
 const library = document.querySelector(".library");
 
 
-//#region    
-
-//#region 
 
 
 function CreateBook() {
@@ -19,7 +24,26 @@ function CreateBook() {
   idCounter++;
 }
 
-btnNewBook.addEventListener("click",CreateBook);
+function showInputs(){
+  const inputs = document.getElementById("form");
+  if(inputs.style.display === "none"){
+    inputs.style.display = "flex";
+  }else{
+    inputs.style.display = "none";
+  }
+}
+
+function test(Name, Author, Pages, Synopsis, Read){
+  console.log(Name, Author, Pages, Synopsis, Read);
+}
+
+btnNewBook.addEventListener("click", showInputs);
+
+btnAddBook.addEventListener("click", CreateBook);
+btnAddBook.addEventListener("click", showInputs);
+btnAddBook.addEventListener("click", test(bookName, bookAuthor, bookPages, bookSynopsis, isAvailable));
+
+
 
 btnDelete.addEventListener("click", function(){
     const child = document.getElementById("book1");
