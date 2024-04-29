ed698849c42d17c74e93bf17a099f494728b8722
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2024-04-29 10:26:31 -0600

    Finished partially, input validation pending

--- btnEvents.js
+++ btnEvents.js
@@ -1,10 +1,13 @@
+
 let controlBooksId = [];
 let idCounter = 0;
 let myLibrary = [];
 let isReadValue;
-const btnDelete = document.querySelector("#btnDelete");
+// const btnDelete = document.querySelector("#btnDelete1");
+const btnDelete = document.querySelector(".btnDelete");
 const btnNewBook = document.querySelector("#btnNewBook");
 const btnAddBook = document.querySelector("#btnAddBook");
+const newBookForm = document.querySelector("#newBookForm");
 const library = document.querySelector(".library");
 const inputs = document.querySelector(".container.inputs")
 
@@ -13,50 +16,142 @@ const bookAuthor = document.querySelector("#bookAuthor");
 const bookPages = document.querySelector("#bookPages");
 const isRead = document.getElementsByName("isAvailable");
 
+function HandleForm(event){
+  event.preventDefault();
+}
+newBookForm.addEventListener("submit", HandleForm);
+
 function GetIsReadValue() {
   for (let i = 0; i < isRead.length; i++) {
     if (isRead[i].checked) {
       isReadValue=parseInt(isRead[i].value);
     }
   }
-  console.log(isReadValue);
-}
+};
 
 function Book (title, author, pagesNo, isRead)     {
     this.title = title;
     this.author = author;
     this.pagesNo = pagesNo;
-    this.isRead = function(isRead){
-        if(isRead === 1){
-            return ("This book has been read");
-        }else if(isRead === 0){
-            return ("This book hasn't been read");
-        }
-    };
+    this.isRead = isRead;
+
+
+    // this.isRead = function(isRead){
+    //     if(isRead === 1){
+    //         return ("This book has been read");
+    //     }else if(isRead === 0){
+    //         return ("This book hasn't been read");
+    //     }
+    // };
+
+    this.isReadValue = function(){
+      if(this.isRead === 1){
+        return ("This book has been read");
+    }else if(this.isRead === 0){
+        return ("This book hasn't been read");
+    }
+    }
 
     this.info = function(){
-        return this.title+", "+this.author+", "+this.pagesNo+", "+this.isRead(isRead)
+        return this.title+", "+this.author+", "+this.pagesNo+", "+this.isReadValue()
     };
-}
+};
 
-function addBookToLibrary(Name, Author, Pages, isAvailible){
-    let book = new Book(Name, Author, Pages, isAvailible)
+
+function addBookToLibrary(Name, Author, Pages, isAvailable){
+    let book = new Book(Name, Author, Pages, isAvailable)
     myLibrary.push(book);
-}
+};
 
 function getAllBooks(library){
     for (let i = 0; i < library.length; i++) {
         const element = library[i].info();
-        console.log(element);
     }
-}
+};
 
 function CreateBook() {
-  const book = document.createElement("div");
-  book.classList.add("book");
-  book.setAttribute("id","book"+idCounter);
-  library.appendChild(book);
-}
+  while(library.firstChild){
+    library.removeChild(library.firstChild);
+    idCounter = 0;
+  }
+
+
+  myLibrary.forEach(element => {
+    //create book and add info
+    const book = document.createElement("div");
+    let bookInfoDiv = document.createElement("div");
+    bookInfoDiv.classList.add("book-info");
+    let bookBtnContainerDiv = document.createElement("div");
+    bookBtnContainerDiv.classList.add("container");
+    let titleText = document.createElement("p");
+    let authorText = document.createElement("p");
+    let pagesText = document.createElement("p");
+    let isReadText = document.createElement("p");
+    titleText.textContent = "Title:"+element.title;
+    authorText.textContent = "Author:"+element.author;
+    pagesText.textContent = "Number of Pages:"+element.pagesNo;
+    isReadText.textContent = "isRead?:"+element.isReadValue();
+
+    //add info to the book
+    bookInfoDiv.appendChild(titleText);
+    bookInfoDiv.appendChild(authorText);
+    bookInfoDiv.appendChild(pagesText);
+    bookInfoDiv.appendChild(isReadText);
+
+    //create delete btn 
+    let btnDelete = document.createElement("button");
+    btnDelete.setAttribute("id", "btnDelete"+idCounter);
+    btnDelete.classList.add("btnDelete");
+    btnDelete.textContent = "Delete";
+    btnDelete.addEventListener("click", function(e){
+      let idNum = e.target.id.substring(9);
+      
+      const child = document.getElementById("book"+idNum);
+      library.removeChild(child);
+      
+      myLibrary.splice(idNum,1);
+    });
+    
+    //create toggle btn
+    let btnToggleRead = document.createElement("button");
+    btnToggleRead.setAttribute("id", "btnToggle"+idCounter);
+    btnToggleRead.classList.add("btnToggle");
+
+    if(element.isRead === 0){
+      btnToggleRead.textContent = "Mark as read";
+    }else if(element.isRead === 1){
+      btnToggleRead.textContent = "Mark as not read";
+    }
+
+    btnToggleRead.addEventListener("click", function(e){
+      let idNum = e.target.id.substring(9);
+
+      let isReadText = document.querySelector('#book'+idNum+' .book-info').children[3];
+      if(myLibrary[idNum].isRead === 0){
+        btnToggleRead.textContent = "Mark as read";
+        myLibrary[idNum].isRead = 1;
+        isReadText.textContent = "isRead?:"+element.isReadValue();
+      }else if(myLibrary[idNum].isRead === 1){
+        btnToggleRead.textContent = "Mark as not read";
+        myLibrary[idNum].isRead = 0;
+        isReadText.textContent = "isRead?:"+element.isReadValue();
+      }
+    });
+    
+    book.appendChild(bookInfoDiv);
+    book.appendChild(bookBtnContainerDiv);
+    bookBtnContainerDiv.appendChild(btnDelete);
+    bookBtnContainerDiv.appendChild(btnToggleRead);
+    
+    book.classList.add("book");
+    book.setAttribute("id","book"+idCounter);
+    
+    library.appendChild(book);
+    idCounter++;
+  });
+  
+};
+
 
 function ShowHideInputs(){
   if(inputs.style.display === "flex"){
@@ -64,19 +159,17 @@ function ShowHideInputs(){
   }else{
     inputs.style.display="flex"; 
   }
-}
+};
 btnNewBook.addEventListener("click",ShowHideInputs);
 
+
+btnAddBook.addEventListener("click",GetIsReadValue);
+btnAddBook.addEventListener("click",function() {
+  addBookToLibrary(bookName.value, bookAuthor.value, parseInt(bookPages.value), isReadValue);  
+  getAllBooks(myLibrary);
+});
 btnAddBook.addEventListener("click",CreateBook);
 btnAddBook.addEventListener("click",ShowHideInputs);
-btnAddBook.addEventListener("click",GetIsReadValue);
-
-btnAddBook.addEventListener("click",function() {
-  addBookToLibrary(bookName.value, bookAuthor.value, bookPages.value, isReadValue);  
-  getAllBooks(myLibrary);
-})
-
-btnDelete.addEventListener("click", function(){
-    const child = document.getElementById("book1");
-    library.removeChild(child);
-})
+btnAddBook.addEventListener("click",function(){
+  newBookForm.reset();
+});
--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 // let myLibrary = [];
-//
+
 // function Book (title, author, pagesNo, isRead)     {
 //     this.title = title;
 //     this.author = author;
 //     this.pagesNo = pagesNo;
-//     this.isRead = function(isRead){
+//     this.isRead = isRead
+
+
+//     this.isReadStatus = function(isRead){
 //         if(isRead === 1){
 //             return ("This book has been read");
 //         }else if(isRead === 0){
 //             return ("This book hasn't been read");
 //         }
 //     };
-//
+    
 //     this.info = function(){
-//         return this.title+", "+this.author+", "+this.pagesNo+", "+this.isRead(isRead)
+//         return this.title+", "+this.author+", "+this.pagesNo+", "+this.isReadStatus(this.isRead)
 //     };
-//
+
 // }
-//
+
 // function addBookToLibrary(book){
 //     myLibrary.push(book);
 // }
-//
+
 // function getAllBooks(library){
 //     for (let i = 0; i < library.length; i++) {
 //         const element = library[i].info();
 //         console.log(element);
 //     }
 // }
-//
-//
-//
+
+
+
 // const book1 = new Book("test1", "owo", "123",0);
 // const book2 = new Book("test2", "uwu", "12qwe3",0);
 // const book3 = new Book("test3", "awa", "12qwe3",0);
@@ -38,9 +41,12 @@
 // const book5 = new Book("test5", "iwi", "12qwe3",0);
 // const book6 = new Book("test6", "unu", "1qwe23",0);
 // const book7 = new Book("test7", "DDD:", "1qwe23",0);
-//
-//
-//
+
+// book1.isRead=1;
+
+
+// console.log(book1.isReadStatus(1));
+
 // addBookToLibrary(book1);
 // addBookToLibrary(book2);
 // addBookToLibrary(book3);
@@ -48,11 +54,11 @@
 // addBookToLibrary(book5);
 // addBookToLibrary(book6);
 // addBookToLibrary(book7);
-//
+
 // getAllBooks(myLibrary);
-//
-//
+// console.log(myLibrary)
+
 // // const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295 Pages",0);
-//
+
 // // // console.log(theHobbit.author);
 // // console.log(theHobbit.info());
